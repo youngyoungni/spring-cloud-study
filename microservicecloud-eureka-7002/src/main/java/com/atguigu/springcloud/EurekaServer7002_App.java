4b69fd9156f1d33831e77e948e60ed832ea291c2
youngyoungni <490604471@qq.com>
2018-11-26 10:50:29 +0800

    Eureka集群搭建

---
+++ microservicecloud-eureka-7002/src/main/java/com/atguigu/springcloud/EurekaServer7002_App.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer		//表示为 Eureka Server 服务器的启动类，接收其他服务注册进来； 
+public class EurekaServer7002_App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer7002_App.class, args);
+	}
+
+}
--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.atguigu.springcloud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,9 @@ public class DeptController {
 	@Autowired
 	private DeptService deptService;
 	
+	@Autowired
+	private DiscoveryClient client;
+	
 	@RequestMapping( value = "/dept/add" , method = RequestMethod.POST)
 	public boolean add(@RequestBody Dept dept) {
 		return deptService.add(dept);
@@ -36,6 +41,22 @@ public class DeptController {
 	@RequestMapping( value = "/dept/list" , method = RequestMethod.GET)
 	public List<Dept> list() {
 		return deptService.list();
+	}
+	
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery() {
+		// 服务发现
+		// 微服务的集合
+		List<String> list = client.getServices();
+		System.out.println("**************"+ list);
+		
+		List<ServiceInstance> instances = client.getInstances("MICROSERVICECLOUD-DEPT");
+		for (ServiceInstance serviceInstance : instances) {
+			System.out.println(serviceInstance.getServiceId() + "\t" + serviceInstance.getHost() + 
+					"\t" + serviceInstance.getPort() + "\t" + serviceInstance.getUri());
+		}
+		
+		return this.client;
 	}
 	
 }
